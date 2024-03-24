d00af653f3c56fef5d8b3aba97c8b0925689d2ab
Your Name <corsiferrao@gmail.com>
2024-03-24 18:34:47 -0400

    python workwing

--- python/main.py
+++ python/main.py
@@ -14,7 +14,8 @@ device = uinput.Device([
 
 def parse_data(data):
     axis = data[0]  # 0 for X, 1 for Y
-    value = int.from_bytes(data[1:3], byteorder='little', signed=True)  # Convert second byte to signed integer
+    value = int.from_bytes(data[1:3], byteorder='little', signed=True)
+    print(f"Received data: {data}")
     print(f"axis: {axis}, value: {value}")
     return axis, value
 
@@ -25,16 +26,21 @@ def move_mouse(axis, value):
     elif axis == 1:  # Y-axis
         device.emit(uinput.REL_Y, value)
 
+
 try:
+    # sync package
     while True:
-        # Read 3 bytes from UART
-        data = ser.read(4)
-        if data[-1] == 0xFF:  # End-of-package indicator
-            axis, value = parse_data(data)
-            move_mouse(axis, value)
-        else:
-            print("Erro no protocolo, dados recebidos:")
-            print(data)
+        print('Waiting for sync package...')
+        while True:
+            data = ser.read(1)
+            if data == b'\xff':
+                break
+
+        # Read 4 bytes from UART
+        data = ser.read(3)
+        axis, value = parse_data(data)
+        move_mouse(axis, value)
+
 except KeyboardInterrupt:
     print("Program terminated by user")
 except Exception as e:
